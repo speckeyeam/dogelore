b0009ca2716a3853600d7f2b634a72125813fa48
speckeyeam <85853453+speckeyeam@users.noreply.github.com>
2023-08-05 00:47:59 -0700

    feat: errors fixed

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,24 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import Google from "@auth/core/providers/google";
-import { id?, gsecret? } from "$env/static/private";
+
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
+let id;
+let gsecret;
+
+// Dynamically import the environment variables
+import("$env/static/private").then(
+  ({ id: importedId, gsecret: importedGsecret }) => {
+    id = importedId || "default-id";
+    gsecret = importedGsecret || "default-gsecret";
+
+    // You can start your tests here or call a function to begin testing
+    // e.g., startTests();
+  }
+);
 export const handle = SvelteKitAuth({
   adapter: PrismaAdapter(prisma),
   providers: [
