b2f278a5efbc4d1c984d2b37a1c0b0209cd1f2e4
speckeyeam <dicklung212@gmail.com>
2023-08-08 00:09:07 -0700

    working on login page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -125,9 +125,22 @@
       {#if popupMenu}
         <button class="popup-background" on:click={togglePopupMenu} />
         <div class="popup-menu" id="popupMenu">
-          <ul>
-            <!-- Add more menu items as needed -->
-          </ul>
+          <div class="popup-div">
+            <h1 class="popup-title">Login</h1>
+            <h2 class="popup-input-tag">username</h2>
+
+            <input class="popup-input" placeholder="username" />
+            <h2 class="popup-input-tag">password</h2>
+            <input class="popup-input" placeholder="password" />
+
+            <button type="button" class="google-sign-in-button">
+              Sign in with Google
+            </button>
+            <div class="popup-buttons">
+              <button class="nav-button popup-button">Submit</button>
+              <button class="nav-button popup-button">Login</button>
+            </div>
+          </div>
         </div>
       {/if}
       {#if data.session}
@@ -483,5 +496,81 @@
     padding: 20px;
     border-radius: 8px;
     z-index: 9999;
+  }
+  .popup-title {
+    font-family: "Jost", sans-serif;
+    text-align: left;
+    font-size: 2.3rem;
+  }
+  .popup-div {
+    text-align: left;
+    display: flex;
+    flex-direction: column;
+    margin: 0;
+  }
+  .popup-input {
+    display: block;
+    margin-right: 8rem;
+    margin-left: 8rem;
+  }
+  .popup-input-tag {
+    margin-top: 0.25rem;
+    margin-right: 8rem;
+    margin-left: 8rem;
+    margin-bottom: 0.1rem;
+    font-weight: 400;
+    font-size: 1.5rem;
+  }
+  .popup-button {
+    width: fit-content !important;
+    right: 0;
+    margin-left: auto;
+    flex-direction: row;
+    display: inline-flex;
+  }
+  .popup-buttons {
+    margin-left: auto;
+  }
+  .google-sign-in-button {
+    width: 15rem;
+    cursor: pointer;
+    transition: background-color 0.3s, box-shadow 0.3s;
+
+    padding: 0.75rem 1rem 0.75rem;
+    border: none;
+    border-radius: 0.4rem;
+    box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.04), 0 1px 1px rgba(0, 0, 0, 0.25);
+
+    color: #757575;
+    font-size: 0.9rem;
+    font-weight: 500;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
+
+    background-image: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTgiIGhlaWdodD0iMTgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PGcgZmlsbD0ibm9uZSIgZmlsbC1ydWxlPSJldmVub2RkIj48cGF0aCBkPSJNMTcuNiA5LjJsLS4xLTEuOEg5djMuNGg0LjhDMTMuNiAxMiAxMyAxMyAxMiAxMy42djIuMmgzYTguOCA4LjggMCAwIDAgMi42LTYuNnoiIGZpbGw9IiM0Mjg1RjQiIGZpbGwtcnVsZT0ibm9uemVybyIvPjxwYXRoIGQ9Ik05IDE4YzIuNCAwIDQuNS0uOCA2LTIuMmwtMy0yLjJhNS40IDUuNCAwIDAgMS04LTIuOUgxVjEzYTkgOSAwIDAgMCA4IDV6IiBmaWxsPSIjMzRBODUzIiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNNCAxMC43YTUuNCA1LjQgMCAwIDEgMC0zLjRWNUgxYTkgOSAwIDAgMCAwIDhsMy0yLjN6IiBmaWxsPSIjRkJCQzA1IiBmaWxsLXJ1bGU9Im5vbnplcm8iLz48cGF0aCBkPSJNOSAzLjZjMS4zIDAgMi41LjQgMy40IDEuM0wxNSAyLjNBOSA5IDAgMCAwIDEgNWwzIDIuNGE1LjQgNS40IDAgMCAxIDUtMy43eiIgZmlsbD0iI0VBNDMzNSIgZmlsbC1ydWxlPSJub256ZXJvIi8+PHBhdGggZD0iTTAgMGgxOHYxOEgweiIvPjwvZz48L3N2Zz4=);
+    background-color: white;
+    background-repeat: no-repeat;
+    background-position: 12px 11px;
+  }
+
+  .google-sign-in-button:hover {
+    box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.04), 0 2px 4px rgba(0, 0, 0, 0.25);
+  }
+
+  .google-sign-in-button:active {
+    background-color: #eeeeee;
+  }
+
+  .google-sign-in-button:active {
+    outline: none;
+    box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.04), 0 2px 4px rgba(0, 0, 0, 0.25),
+      0 0 0 3px #c8dafc;
+  }
+
+  .google-sign-in-button:disabled {
+    filter: grayscale(100%);
+    background-color: #ebebeb;
+    box-shadow: 0 -1px 0 rgba(0, 0, 0, 0.04), 0 1px 1px rgba(0, 0, 0, 0.25);
+    cursor: not-allowed;
   }
 </style>
