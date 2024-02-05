73fc7836f28910663bd970d28d1b34215477502b
speckeyeam <dicklung212@gmail.com>
2024-02-04 23:14:34 -0800

    started working templates

---
+++ src/routes/templates/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import NavBar from "$lib/components/NavBar.svelte";
+  export let data;
+  import "$lib/styles/style.css";
+</script>
+
+<NavBar {data} />
+<div class="template-container"></div>
+<hr />
+
+<button class="create-template">+</button>
+<a href="/backgrounds">backgrounds</a>
+<a href="/characters">characters</a>
